2212167c55e35b0b9870dba5835f17d6d8b6be9b
Abdul Raffi <rafiimaut@gmail.com>
2016-03-29 15:32:10 +0700

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -32,7 +32,7 @@ public class Fibonacci {
         }
         for (int i = 0; i < n; i++) {
             
-            System.out.print(fib[i] + " ");
+            System.out.print(jum);
         }
     }
 }
